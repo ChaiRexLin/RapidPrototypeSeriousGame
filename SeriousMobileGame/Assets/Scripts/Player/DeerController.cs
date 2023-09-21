a086d6ca346b1799e2466aed2481f1654cb9a8da
ChaiRexLin <143537435+ChaiRexLin@users.noreply.github.com>
2023-09-21 00:41:46 -0600

    swipe up to jump and touch to interact

--- SeriousMobileGame/Assets/Scripts/Player/DeerController.cs
+++ SeriousMobileGame/Assets/Scripts/Player/DeerController.cs
@@ -6,10 +6,16 @@ public class DeerController : MonoBehaviour {
 
 
     [SerializeField] private float movementSpeed = 10f;
-    [SerializeField] private float rotationSpeed = 90f;
+    [SerializeField] private float rotationSpeed = 180f;
+    [SerializeField] private float jumpPower = 250f;
+    [SerializeField] private float jumpCooldown = 1f;
     [SerializeField] private Rigidbody deerRigidBody;
 
     private float tilt;
+    private float jumpTimer = 0f;
+
+    private Vector2 startTouchPosition;
+    private Vector2 endTouchPosition;
 
     private void Start() {
         Input.gyro.enabled = true;
@@ -20,28 +26,67 @@ public class DeerController : MonoBehaviour {
         //transform.rotation = Input.gyro.attitude;
 
         tilt += Input.acceleration.x * rotationSpeed * Time.deltaTime;
-        //Vector3 movement = transform.forward * movementSpeed * -Input.acceleration.z * Time.deltaTime;
-        //deerRigidBody.MovePosition(deerRigidBody.position + movement);
-
-
 
         Quaternion targetRotation = Quaternion.Euler(0, tilt, 0);
         deerRigidBody.rotation = Quaternion.Lerp(deerRigidBody.rotation, targetRotation, Time.deltaTime);
-        //deerRigidBody.MoveRotation(Quaternion.Lerp(deerRigidBody.rotation, targetRotation, Time.deltaTime));
 
 
         // touch
-
         if (Input.touchCount > 0 ) {
-            Touch touch = Input.GetTouch(0);
-            Vector2 touchPositionNormalized = touch.position.normalized;
-            Debug.Log(touchPositionNormalized.y - 0.15f);
 
-            if (touchPositionNormalized.x < .8f) { return; }
+            Touch forwardTouch = Input.GetTouch(0);
+            Vector2 touchPositionNormalized = forwardTouch.position.normalized;
+            //Debug.Log(touchPositionNormalized.y - 0.15f);
 
-            Vector3 movement = transform.forward * movementSpeed * (touchPositionNormalized.y - 0.15f) * 2 * Time.deltaTime;
+            if (touchPositionNormalized.x > .8f) {
+                Vector3 movement = transform.forward * movementSpeed * (touchPositionNormalized.y - 0.15f) * 2 * Time.deltaTime;
 
-            deerRigidBody.MovePosition(deerRigidBody.position + movement);
+                deerRigidBody.MovePosition(deerRigidBody.position + movement);
+            }
+
+            // draw ray
+            for (int i = 0; i < Input.touchCount; i++) {
+                Touch rayTouch = Input.GetTouch(i);
+                if (rayTouch.position.normalized.x < .8f) {
+                    Vector3 rayTouchPosition = rayTouch.position;
+                    rayTouchPosition.z = 100f;
+                    rayTouchPosition = Camera.main.ScreenToWorldPoint(rayTouchPosition);
+                    Debug.DrawRay(Camera.main.transform.position, rayTouchPosition - Camera.main.transform.position, Color.blue);
+
+                    if (rayTouch.phase == TouchPhase.Ended) {
+                        Ray ray = Camera.main.ScreenPointToRay(rayTouch.position);
+                        RaycastHit hit;
+
+                        if (Physics.Raycast(ray, out hit, 10)) {
+                            Debug.Log(hit.transform.name);
+                        }
+                    }
+                }
+
+            }
+
+            //swipe
+            if (jumpTimer < jumpCooldown) {
+                jumpTimer += Time.deltaTime;
+                return;
+            }
+
+            for (int i = 0; i < Input.touchCount; i++) {
+                Touch jumpTouch = Input.GetTouch(i);
+                if (jumpTouch.position.normalized.x < .8f) {
+                    if (jumpTouch.phase == TouchPhase.Began) {
+                        startTouchPosition = jumpTouch.position;
+                    }
+                    if (jumpTouch.phase == TouchPhase.Ended) {
+                        endTouchPosition = jumpTouch.position;
+                        if (endTouchPosition.y - startTouchPosition.y > 0.5f) {
+                            deerRigidBody.AddForce(Vector3.up * jumpPower);
+                            jumpTimer = 0f;
+                        }
+                    }
+                }
+            }
+
         }
 
     }
