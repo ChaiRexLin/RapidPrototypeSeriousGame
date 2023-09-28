74be09969d546db798a930b0670245995cdb1732
Edmund0x0 <61939268+Edmund0x0@users.noreply.github.com>
2023-09-28 12:04:57 -0600

    fix bug in deer control

--- SeriousMobileGame/Assets/Scripts/Player/DeerController.cs
+++ SeriousMobileGame/Assets/Scripts/Player/DeerController.cs
@@ -1,110 +1,130 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class DeerController : MonoBehaviour {
+public class DeerController : MonoBehaviour
+    {
 
 
-    [SerializeField] private float movementSpeed = 10f;
-    [SerializeField] private float foliageMovementMultiplier = 0.8f;
-    [SerializeField] private float rotationSpeed = 180f;
-    [SerializeField] private float jumpPower = 250f;
-    [SerializeField] private float jumpCooldown = 1f;
-    [SerializeField] private Rigidbody deerRigidBody;
+        [SerializeField] private float movementSpeed = 10f;
+        [SerializeField] private float rotationSpeed = 180f;
+        [SerializeField] private float jumpPower = 250f;
+        [SerializeField] private float foliageMovementMultiplier = 0.8f;
+        [SerializeField] private float jumpCooldown = 1f;
+        [SerializeField] private Rigidbody deerRigidBody;
 
-    private float tilt;
-    private float jumpTimer = 0f;
-    private float movementMultiplier = 1f;
+        private float tilt;
+        private float jumpTimer = 0f;
+        private float movementMultiplier = 1f;
 
-    private Vector2 startTouchPosition;
-    private Vector2 endTouchPosition;
+        private Vector2 startTouchPosition;
+        private Vector2 endTouchPosition;
 
-    private void Start() {
-        Input.gyro.enabled = true;
-    }
-
-    private void Update() {
-        //Debug.Log(Input.gyro.attitude);
-        //transform.rotation = Input.gyro.attitude;
-
-        tilt += Input.acceleration.x * rotationSpeed * Time.deltaTime;
-
-        Quaternion targetRotation = Quaternion.Euler(0, tilt, 0);
-        deerRigidBody.rotation = Quaternion.Lerp(deerRigidBody.rotation, targetRotation, Time.deltaTime);
-
-        if (jumpTimer < jumpCooldown)
+        private void Start()
         {
-            jumpTimer += Time.deltaTime;
-            return;
+            Input.gyro.enabled = true;
         }
 
-        // touch
-        if (Input.touchCount > 0 ) {
+        private void Update()
+        {
+            //Debug.Log(Input.gyro.attitude);
+            //transform.rotation = Input.gyro.attitude;
 
-            Touch forwardTouch = Input.GetTouch(0);
-            Vector2 touchPositionNormalized = forwardTouch.position.normalized;
-            //Debug.Log(touchPositionNormalized.y - 0.15f);
+            tilt += Input.acceleration.x * rotationSpeed * Time.deltaTime;
 
-            if (touchPositionNormalized.x > .8f) {
-                Vector3 movement = transform.forward * movementSpeed * movementMultiplier * (touchPositionNormalized.y - 0.15f) * 2 * Time.deltaTime;
+            Quaternion targetRotation = Quaternion.Euler(0, tilt, 0);
+            deerRigidBody.rotation = Quaternion.Lerp(deerRigidBody.rotation, targetRotation, Time.deltaTime);
 
-                deerRigidBody.MovePosition(deerRigidBody.position + movement);
+            if (jumpTimer < jumpCooldown)
+            {
+                jumpTimer += Time.deltaTime;
+                return;
             }
 
-            // draw ray
-            for (int i = 0; i < Input.touchCount; i++) {
-                Touch rayTouch = Input.GetTouch(i);
-                if (rayTouch.position.normalized.x < .8f) {
-                    Vector3 rayTouchPosition = rayTouch.position;
-                    rayTouchPosition.z = 100f;
-                    rayTouchPosition = Camera.main.ScreenToWorldPoint(rayTouchPosition);
-                    Debug.DrawRay(Camera.main.transform.position, rayTouchPosition - Camera.main.transform.position, Color.blue);
+            // touch
+            if (Input.touchCount > 0)
+            {
 
-                    if (rayTouch.phase == TouchPhase.Ended) {
-                        Ray ray = Camera.main.ScreenPointToRay(rayTouch.position);
-                        RaycastHit hit;
+                Touch forwardTouch = Input.GetTouch(0);
+                Vector2 touchPositionNormalized = new Vector2(forwardTouch.position.x / Screen.width, forwardTouch.position.y / Screen.height);
+                //Debug.Log(touchPositionNormalized.y - 0.15f);
 
-                        if (Physics.Raycast(ray, out hit, 10)) {
-                            Debug.Log(hit.transform.name);
+                if (touchPositionNormalized.x > .8f)
+                {
+                    Vector3 movement = transform.forward * movementSpeed * movementMultiplier * (touchPositionNormalized.y - 0.15f) * 2 * Time.deltaTime;
+
+                    deerRigidBody.MovePosition(deerRigidBody.position + movement);
+                }
+
+                // draw ray
+                for (int i = 0; i < Input.touchCount; i++)
+                {
+                    Touch rayTouch = Input.GetTouch(i);
+                    if (rayTouch.position.x/ Screen.width < .8f)
+                    {
+                        Vector3 rayTouchPosition = rayTouch.position;
+                        rayTouchPosition.z = 100f;
+                        rayTouchPosition = Camera.main.ScreenToWorldPoint(rayTouchPosition);
+                        Debug.DrawRay(Camera.main.transform.position, rayTouchPosition - Camera.main.transform.position, Color.blue);
+
+                        if (rayTouch.phase == TouchPhase.Ended)
+                        {
+                            Ray ray = Camera.main.ScreenPointToRay(rayTouch.position);
+                            RaycastHit hit;
+
+                            if (Physics.Raycast(ray, out hit, 10))
+                            {
+                                Debug.Log(hit.transform.name);
+                            }
+                        }
+                    }
+
+                }
+
+                //swipe
+
+                for (int i = 0; i < Input.touchCount; i++)
+                {
+                    Touch jumpTouch = Input.GetTouch(i);
+                    if (jumpTouch.position.x / Screen.width < .8f)
+                    {
+                        if (jumpTouch.phase == TouchPhase.Began)
+                        {
+                            startTouchPosition = jumpTouch.position;
+                        }
+                        if (jumpTouch.phase == TouchPhase.Ended)
+                        {
+                            endTouchPosition = jumpTouch.position;
+                            if (endTouchPosition.y - startTouchPosition.y > 300f)
+                            {
+                                deerRigidBody.AddForce(Vector3.up * jumpPower);
+                                jumpTimer = 0f;
+                            }
                         }
                     }
                 }
 
             }
 
-            //swipe
+        }
 
-            for (int i = 0; i < Input.touchCount; i++) {
-                Touch jumpTouch = Input.GetTouch(i);
-                if (jumpTouch.position.normalized.x < .8f) {
-                    if (jumpTouch.phase == TouchPhase.Began) {
-                        startTouchPosition = jumpTouch.position;
-                    }
-                    if (jumpTouch.phase == TouchPhase.Ended) {
-                        endTouchPosition = jumpTouch.position;
-                        if (endTouchPosition.y - startTouchPosition.y > 0.5f) {
-                            deerRigidBody.AddForce(Vector3.up * jumpPower);
-                            jumpTimer = 0f;
-                        }
-                    }
-                }
+        private void OnTriggerEnter(Collider collider)
+        {
+            if (collider.gameObject.layer == LayerMask.NameToLayer("Foliage"))
+            {
+                movementMultiplier = foliageMovementMultiplier;
             }
-
         }
 
-    }
-
-
-
-    private void OnTriggerEnter(Collider collider) {
-        if (collider.gameObject.layer == LayerMask.NameToLayer("Foliage")) {
-            movementMultiplier = foliageMovementMultiplier;
+        private void OnTriggerExit(Collider collider)
+        {
+            if (collider.gameObject.layer == LayerMask.NameToLayer("Foliage"))
+            {
+                movementMultiplier = 1f;
+            }
         }
     }
 
-    private void OnTriggerExit(Collider collider) {
-        if (collider.gameObject.layer == LayerMask.NameToLayer("Foliage")) {
-            movementMultiplier = 1f;
-        }
-    }
-}
+
+       
